352ce86301bd5fcbddecc071559cb721df8059d4
iago-costa <43588158+iago-costa@users.noreply.github.com>
2023-09-28 23:22:04 -0300

    feat: Updates in init.lua to load diff config to NeoVim in VSCode

--- init.lua
+++ init.lua
@@ -13,83 +13,86 @@ require('plugins')
 -- Load mason to install LSP servers
 require("mason").setup()
 
--- Load lsp configuration from setup_lsp.lua
-require('setup_lsp')
+if not vim.g.vscode then
+    -- Load lsp configuration from setup_lsp.lua
+    require('setup_lsp')
 
--- Load lsp configuration for dart from lsp_dart.lua
-require('lsp_dart')
+    -- Load lsp configuration for dart from lsp_dart.lua
+    require('lsp_dart')
 
--- Load lsp configuration for flutter from lsp_flutter.lua
-require('lsp_flutter')
+    -- Load lsp configuration for flutter from lsp_flutter.lua
+    require('lsp_flutter')
 
--- Load lsp configuration auto-completion from lsp_cmp.lua
-require('lsp_cmp')
+    -- Load lsp configuration for rust from lsp_rust.lua
+    require('lsp_rust')
 
--- Load lsp configuration for rust from lsp_rust.lua
-require('lsp_rust')
+    -- Load lsp configuration for python from lsp_python.lua
+    require('lsp_python')
 
--- Load lsp configuration for python from lsp_python.lua
-require('lsp_python')
+    -- Load lsp configuration for lua from lsp_css.lua
+    require('lsp_css')
 
--- Load lsp configuration for lua from lsp_css.lua
-require('lsp_css')
+    -- Load lsp configuration for lua from lsp_javascript.lua
+    require('lsp_javascript')
 
--- Load lsp configuration for lua from lsp_javascript.lua
-require('lsp_javascript')
+    -- Load lsp configuration for lua from lsp_lua.lua
+    require('lsp_lua')
 
--- Load lsp configuration for lua from lsp_lua.lua
-require('lsp_lua')
+    -- Load treesitter configuration from treesitter.lua
+    require('tree_sitter')
 
--- Load undotree configuration for lua from undotree.lua
-require('undo_tree')
+    -- Load harpoon configuration for lua from harpoon.lua
+    require('setup_harpoon')
 
--- Enable setup_nvim_tree.lua
-require('setup_nvim_tree')
+    -- Load undotree configuration for lua from undotree.lua
+    require('undo_tree')
 
--- Load toggleterm configuration from toggle_term.lua
-require('toggle_term')
+    -- Enable setup_nvim_tree.lua
+    require('setup_nvim_tree')
 
--- Load telescope configuration from telescope.lua
-require('setup_telescope')
+    -- Load toggleterm configuration from toggle_term.lua
+    require('toggle_term')
 
--- Load vim-matchup configuration from vim_matchup.lua
-require('vim_matchup')
+    -- Load telescope configuration from telescope.lua
+    require('setup_telescope')
+
+    -- Load vim-matchup configuration from vim_matchup.lua
+    require('vim_matchup')
+
+    -- Load refactoring configuration for lua from refactoring.lua
+    require('setup_refactoring')
+
+    -- Load trouble configuration for lua from trouble.lua
+    require('setup_trouble')
+
+    -- Load setup_auto_save.lua
+    require('setup_auto_save')
+
+    -- Load setup_lazy_git.lua
+    require('setup_lazy_git')
+
+    -- Load setup_doc_keymap.lua
+    require('setup_doc_keymap')
+
+    -- Load setup_git_signs.lua
+    require('setup_git_signs')
+
+    -- Load setup_ctrlsf.lua
+    require('setup_ctrlsf')
+
+    -- Load setup_command_completion.lua
+    require('setup_command_completion')
+
+    -- Load setup_nvim_ufo.lua
+    require('setup_nvim_ufo')
+end
 
 -- Load commentary configuration from commentary.lua
 require('commentary')
 
--- Load treesitter configuration from treesitter.lua
-require('tree_sitter')
+-- Load lsp configuration auto-completion from lsp_cmp.lua
+require('lsp_cmp')
 
--- Load harpoon configuration for lua from harpoon.lua
-require('setup_harpoon')
-
--- Load refactoring configuration for lua from refactoring.lua
-require('setup_refactoring')
-
--- Load trouble configuration for lua from trouble.lua
-require('setup_trouble')
-
--- Load setup_auto_save.lua
-require('setup_auto_save')
-
--- Load setup_lazy_git.lua
-require('setup_lazy_git')
-
--- Load setup_doc_keymap.lua
-require('setup_doc_keymap')
-
--- Load setup_git_signs.lua
-require('setup_git_signs')
-
--- Load setup_ctrlsf.lua
-require('setup_ctrlsf')
-
--- Load setup_command_completion.lua
-require('setup_command_completion')
-
--- Load setup_nvim_ufo.lua
-require('setup_nvim_ufo')
 
 -- Load custom commands and options from global_cmds.lua, global_opts.lua and global_keys.lu
 require('global_cmds')
--- lua/lsp_python.lua
+++ lua/lsp_python.lua
@@ -1,9 +1,25 @@
 local nvim_lsp = require('lspconfig')
 
 -- Configure Pyright for Python
-nvim_lsp.pyright.setup{
-
+nvim_lsp.pyright.setup {
+    cmd = { "pyright" },
+    filetypes = { "python" },
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "workspace",
+                typeCheckingMode = "basic",
+                stubPath = "/home/gup/.local/share/nvim/lspinstall/python/node_modules/pyright-stdlib"
+            }
+        }
+    }
 }
+
+
+-- Configure pylama for pyright
+
 
 -- Configure pylsp for Python
 nvim_lsp.pylsp.setup{
@@ -12,19 +28,21 @@ nvim_lsp.pylsp.setup{
     settings = {
         pylsp = {
             plugins = {
-                pylint = {
+                -- pylint = {
+                --     enabled = true,
+                --     executable = "pylint",
+                -- },
+                -- pyflakes = {
+                --     enabled = true,
+                --     executable = "pyflakes",
+                --     args = {}
+                -- },
+                pylama = {
                     enabled = true,
-                    executable = "pylint",
-                },
-                pyflakes = {
-                    enabled = true,
-                    executable = "pyflakes",
+                    executable = "pylama",
                     args = {}
                 },
             },
         },
     },
 }
-
-
-
