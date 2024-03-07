905c1ecc3b7e80c96056f55d6f9d9f82da92d558
iago-costa <iagocostati@gmail.com>
2024-03-07 04:33:40 -0300

    chore: update commit.sh

--- commit.sh
+++ commit.sh
@@ -4,52 +4,20 @@ if [ -z "$(git status --porcelain)" ]; then
     exit 0
 fi
 
+echo "Have changes to commit"
+# get list of files to commit
+list=$(git status -s | grep '' | awk '{print $2}')
 
-# check if there are any tracked files to commit A D M R C U
-if [ -z "$(git diff --staged --name-only | awk 'NR>=0 && NR<=999')" ]; then 
-    echo "No tracked files to commit"
-else
-    echo "Tracked files found. Please enter commit message"
-    # get the filename of the file
-    list=$(git diff --staged --name-only | awk 'NR>=0 && NR<=999')
+# commit all files one by one
+for file in $list
+do
+    git add $file
     
     # get the diff of the file
     git diff --staged -- $file
 
-    # commit all files one by one
-    for file in $list
-    do
-        echo "Want to commit $file now? (y/n)"
-        read answer
-        if [ "$answer" == "y" ]; then
-            git commit -e
-        else
-            exit 0
-        fi
-    done
-fi
+    git commit -e
+done
 
 
-# check if there are any file to commit
-if [ -z "$(git status --porcelain)" ]; then 
-    echo "No changes to commit"
-    exit 0
-else
-    echo "There are still changes to commit"
-    list=$(git status --porcelain | awk '{print $2}')
-    for file in $list
-    do
-        git diff HEAD $file
-
-        echo "Want to commit $file now? (y/n)"
-        read answer
-        if [ "$answer" == "y" ]; then
-            git add $file
-            # add git diff HEAD $file to the commit message with -e and commented out
-            git commit -e
-        else
-            exit 0
-        fi
-    done
-fi
 
