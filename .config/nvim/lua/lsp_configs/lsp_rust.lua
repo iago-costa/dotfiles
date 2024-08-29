6cab43a578b74be8cfcda80c45d409f7b66c8c62
iago-costa <iagocostati@gmail.com>
2024-08-28 21:18:56 -0300

    chore: update nvim plugin lsp rust

--- .config/nvim/lua/lsp_configs/lsp_rust.lua
+++ .config/nvim/lua/lsp_configs/lsp_rust.lua
@@ -81,8 +81,6 @@ require('crates').setup {
   curl_args = { "-sL", "--retry", "1" },
   max_parallel_requests = 80,
   expand_crate_moves_cursor = true,
-  -- open_programs = { "xdg-open", "open" },
-  disable_invalid_feature_diagnostic = false,
   enable_update_available_warning = true,
   text = {
     loading = "   Loading",
